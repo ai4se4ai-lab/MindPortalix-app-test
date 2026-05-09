c576dacbbbf629661a44d581841ba31273fc4cfd
ai4se4ai-lab <ai4se4ai@gmail.com>
2026-05-09 15:28:53 -0700

    Refactor Flask application to remove traceroute check and add print function

--- test4.py
+++ test4.py
@@ -1,15 +1,5 @@
-from flask import Flask, request
-import subprocess
 
-app = Flask(__name__)
-
-@app.route('/check')
-def check():
-    target = request.args.get('target')
-
-    result = subprocess.getoutput(f"traceroute {target}")
-
-    return f"<pre>{result}</pre>"
-
+def printMe():
+    print("Hello World")
 if __name__ == '__main__':
-    app.run(port=8080)
+    printMe()
