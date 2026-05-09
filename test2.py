e2e65129ccc64811b1a04a0178aec8e468ec8bad
ai4se4ai-lab <ai4se4ai@gmail.com>
2026-05-09 14:25:22 -0700

    Add feedback route to Flask application

---
+++ test2.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+PAGE = """
+<html>
+<body>
+    <h2>Feedback</h2>
+    <div>%s</div>
+</body>
+</html>
+"""
+
+@app.route('/feedback')
+def feedback():
+    text = request.args.get('text', '')
+
+    return render_template_string(PAGE % text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
