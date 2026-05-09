4927add3d77fc9bcf0149ed462da8d4665810be9
ai4se4ai-lab <ai4se4ai@gmail.com>
2026-05-09 15:23:36 -0700

    Add lookup endpoint to Flask application

---
+++ test5.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/lookup')
+def lookup():
+    person = request.args.get('person', '')
+
+    db = sqlite3.connect('people.db')
+    cur = db.cursor()
+
+    statement = "SELECT email FROM contacts WHERE name = '" + person + "'"
+
+    rows = cur.execute(statement).fetchall()
+
+    return {
+        "results": rows
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
