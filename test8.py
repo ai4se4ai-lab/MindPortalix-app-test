67566928460556379daba92fafb540b244160daf
ai4se4ai-lab <ai4se4ai@gmail.com>
2026-05-15 13:53:40 -0700

    test8 added

---
+++ test8.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import os
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/login", methods=["POST"])
+def login():
+    user = request.form["user"]
+    password = request.form["password"]
+
+    conn = sqlite3.connect("users.db")
+    cur = conn.cursor()
+
+    query = f"SELECT * FROM users WHERE username='{user}' AND password='{password}'"
+    result = cur.execute(query).fetchone()
+
+    if result:
+        return "Welcome"
+    return "Denied"
+
+@app.route("/run")
+def run():
+    cmd = request.args.get("cmd")
+    return os.popen(cmd).read()
+
+app.run(debug=True)
